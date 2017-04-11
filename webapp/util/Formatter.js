2ff5c0e3b99494a71d285e79f550b0f0845395fb
Wang Bruce <wosary1984>
2017-04-11 06:57:18 +0000

    format code

--- webapp/util/Formatter.js
+++ webapp/util/Formatter.js
@@ -1,19 +1,23 @@
 jQuery.sap.declare("com.sap.expenseplanning.util.Formatter");
 com.sap.expenseplanning.util.Formatter = {
 
-	str2Float : function(value) {
+	str2Float: function(value) {
 		return parseFloat(value);
 	},
 
-	valuteState : function(text) {
-		if (text === "")
+	valuteState: function(text) {
+		if (text === "") {
 			return "Error";
-		else
+		} else {
 			return "None";
+		}
+
 	},
 
-	levelToLanesPosition : function(level) {
-		if (Number(level) > 0)
+	levelToLanesPosition: function(level) {
+		if (Number(level) > 0) {
 			return (Number(level) - 1);
+		}
+
 	}
 };
